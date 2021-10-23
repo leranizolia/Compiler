900acf48fd31eec1451bb7f3553f4de94e0bdbd0
Валерия Низоля <leranizolya@yandex.ru>
2021-10-23 16:11:32 +0300

    move to lib

--- mc/Program.cs
+++ mc/Program.cs
@@ -33,11 +33,10 @@ namespace Minsk
                 }
 
                 var syntaxTree = SyntaxTree.Parse(line);
-                var binder = new Binder();
-                var boundExpression = binder.BindExpression(syntaxTree.Root);
+                var compilation = new Compilation(syntaxTree);
+                var result = compilation.Evaluate();
 
-
-                IReadOnlyList<string> diagnostics = syntaxTree.Diagnostics.Concat(binder.Diagnostics).ToArray();
+                var diagnostics = result.Diagnostics;
 
                 if (showTree)
                 {
@@ -49,9 +48,7 @@ namespace Minsk
 
                 if (!diagnostics.Any())
                 {
-                    var e = new Evaluator(boundExpression);
-                    var result = e.Evaluate();
-                    Console.WriteLine(result);
+                    Console.WriteLine(result.Value);
                 }
                 else
                 {
