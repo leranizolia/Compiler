37031ddb6ec1067c2ffb751ab018792df05bf700
Валерия Низоля <leranizolya@yandex.ru>
2021-12-05 18:03:13 +0300

    syntax facts tests and wip for parser tests

--- mc/Minsk.Tests/CodeAnalysis/Syntax/LexerTests.cs
+++ mc/Minsk.Tests/CodeAnalysis/Syntax/LexerTests.cs
@@ -5,7 +5,7 @@ using Xunit;
 
 namespace Minsk.Tests.CodeAnalysis.Syntax
 {
-    public class LexerTest
+    public class LexerTests
     {
         [Theory]
         [MemberData(nameof(GetTokensData))]
